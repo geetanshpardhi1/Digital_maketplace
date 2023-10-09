a77d8fd9a074f93f1d0a9287f384a780cd466a40
geetanshpardhi1 <darkwolfcreation7552@gmail.com>
2023-10-09 12:40:18 +0530

    added edit functionality for products

--- myapp/urls.py
+++ myapp/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('failed/',views.payment_failed_view,name='failed'),
     path('api/checkout-session/<int:id>/',views.create_checkout_session,name='api_checkout_session'),
     path('createproduct/',views.create_product,name='create_product'),
+    path('editproduct/<int:id>/',views.product_edit,name='editproduct')
     
 ]
 
